1f38d845c0b4f6c85ac63bafc4caa8624293de4c
solome <solomeyemane53@gmail.com>
2022-10-17 04:41:07 -0700

    compute the absolute value of intiger

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,22 +1,15 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code
+ * _abs: computes the absolute value of number
+ * @n the number to be computed
  *
- * Return  Always 0
+ * Return: the absolute value of number
  */
-int main(void)
+int _abs(int n)
 {
-	int r;
-
-	r = _abs(-1);
-	printf("%d\n" , r);
-	r = _abs(0);
-	printf("%d\n" , r);
-	r = _abs(1);
-	printf("%d\n" , r);
-	r = _abs(-98);
-	printf("%d\n" , r);
-	return (0);
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
 }
