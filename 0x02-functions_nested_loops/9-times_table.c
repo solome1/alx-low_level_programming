27d24d8e71eae2bedd6901c19bc0195e52736d8e
Solome <96201149+solome1@users.noreply.github.com>
2022-10-19 11:41:22 -0700

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,22 +2,21 @@
 
 /**
  * times_table: prints the 9 time table starting from 0
- *
  * Return: times tabel starting from 0 to 9 times 
  */
 void times_table(void)
 {
 	int row, col, mul;
 
-	for(row = 0; row <= 9; row++)
+	for (row = 0; row <= 9; row++)
 	{
 		_putchar('0');
 		_putchar(',');
 		_putchar(' ');
-		for(col = 1; col <= 9; col++)
+		for (col = 1; col <= 9; col++)
 		{
 			mul = (row * col);
-			if((mul / 10) > 0)
+			if ((mul / 10) > 0)
 			{
 				_putchar((mul / 10) + '0');
 			}
@@ -27,7 +26,7 @@ void times_table(void)
 			}
 			_putchar((mul % 10) + '0');
 
-			if(col < 9)
+			if (col < 9)
 			{
 				_putchar(',');
 				_putchar(' ');
